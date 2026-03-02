73e130f31110673e80180c3a8fbe8f036cca1a9a
Faiz2979 <faizannabil877@gmail.com>
2026-03-02 13:25:13 +0700

    feat: update Tailwind CSS configuration with custom font and color settings; remove unused font styles from globals.css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,28 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-koho)"],
+      },
+      colors: {
+        primary: {
+          DEFAULT: "#c51f1f",
+          dark: "#991b1b",
+          deep: "#7f1d1d",
+        },
+        black: {
+          DEFAULT: "#0f0f0f",
+          soft: "#1a1a1a",
+          border: "#262626",
+        },
+        white: {
+          DEFAULT: "#f5f5f5",
+          soft: "#e5e5e5",
+          border: "#d4d4d4",
+        }
+      },
+    }
   },
   plugins: [],
 }
